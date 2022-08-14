ba5979c3c516dbf2690c258893520d0e6f556f07
zehongharryqu <harry_qu00@yahoo.ca>
2022-08-14 00:07:48 -0400

    alch

--- alch.py
+++ alch.py
@@ -1,14 +1,12 @@
 import pyautogui
 import time
 
-# tuning - sell
-BIN_FIRST_POT_X = 575
-BIN_FIRST_POT_Y = 525
+# Mac
+# FIRST_INV = (577, 522)
+# BREW_TOP = (720, 316)
+FIRST_INV = (577, 522)
+BREW_TOP = (720, 316)
 NEXT_SLOT = 36
-PRICE_X = 720
-PRICE_Y = 390
-ONE_HOUR = True
-PRICE = '59999'
 
 
 def brew_pots():
@@ -19,24 +17,41 @@ def brew_pots():
         for j in range(9):
             if i == 3 and j == 6:
                 return
-            pyautogui.moveTo(540 + 36 * j, 450 + 36 * i)
+            pyautogui.moveTo(FIRST_INV[0] + NEXT_SLOT * j, FIRST_INV[1] + NEXT_SLOT * i)
             shift_click()
             bottles_in += 1
             if bottles_in == 3:
-                pyautogui.moveTo(755, 565)  # wart
+                pyautogui.moveTo(FIRST_INV[0] + NEXT_SLOT * 6, FIRST_INV[1] + NEXT_SLOT * 3)  # add wart
                 shift_click()
                 time.sleep(22)
-                pyautogui.moveTo(680, 240)  # top
+                pyautogui.moveTo(*BREW_TOP)  # remove wart
                 shift_click()
-                pyautogui.moveTo(755 + 36, 565)  # sugar
+                pyautogui.moveTo(FIRST_INV[0] + NEXT_SLOT * 7, FIRST_INV[1] + NEXT_SLOT * 3)  # add sugar
                 shift_click()
                 time.sleep(22)
-                pyautogui.moveTo(680, 240)  # top
+                pyautogui.moveTo(*BREW_TOP)  # remove sugar
                 shift_click()
                 for k in range(3):
-                    pyautogui.moveTo(610 + 72 * k, 350)  # pot
+                    pyautogui.moveTo(BREW_TOP[0] + NEXT_SLOT * (-2 + 2 * k), BREW_TOP[1] + NEXT_SLOT * 3)  # pot
                     shift_click()
                 bottles_in = 0
+
+
+def sell_pots():
+    time.sleep(2)
+    pyautogui.write('e9', interval=0.5)
+    pyautogui.click()
+    time.sleep(0.5)
+    pyautogui.moveTo(BREW_TOP[0], BREW_TOP[1]+NEXT_SLOT)
+    time.sleep(0.5)
+    pyautogui.click()
+    time.sleep(0.5)
+    for i in range(4):
+        for j in range(9):
+            if i == 3 and j == 6:
+                return
+            pyautogui.click(FIRST_INV[0] + NEXT_SLOT * j, FIRST_INV[1] + NEXT_SLOT * i)
+            time.sleep(0.5)
 
 
 def shift_click():
@@ -46,6 +61,5 @@ def shift_click():
 
 
 if __name__ == '__main__':
-    # brew_pots()
+    brew_pots()
     sell_pots()
-    
